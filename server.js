5fc5cd80c455da27846444579754518916fb481d
Samuel Avila <samuel.avila@tekever.com>
2025-08-22 20:36:53 +0100

    Teste run

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Servidor Node.js está rodando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
