a579384f2beb7a8063fd35eed236c150edeefedd
Samuel Avila <samuel.avila@tekever.com>
2025-08-22 21:32:32 +0100

    VAI GABIREL

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Servidor Node.js está rodando msgfdsadaddsad! other ');
+  res.send('AGORA VAI, GO GABRIEL');
 });
 
 app.listen(PORT, () => {
