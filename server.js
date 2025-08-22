cf13f6bf86935462d3e15de6a5d2b74656bbbccd
Samuel Avila <samuel.avila@tekever.com>
2025-08-22 21:36:47 +0100

    faodsa

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('AGORA VAI, GO GABRIEL');
+  res.send('AGORA VAI, GO GABRIELdsad');
 });
 
 app.listen(PORT, () => {
